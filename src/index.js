8af8c8a5c4436889aa852037934c058ac4d41118
teemutervo <103576935+teemutervo@users.noreply.github.com>
2022-06-10 15:05:48 +0300

    Minor fixes to json

--- src/index.js
+++ src/index.js
@@ -11,7 +11,18 @@ if (document.readyState !== "loading") {
 
 // Ensimmäinen koira
 
-function initializeCode() {
+async function initializeCode() {
+  async function dogBreed1() {
+    let url = "https://dog.ceo/api/breed/schipperke/images/random";
+    let response = await fetch(url);
+    let dogBreed = await response.json();
+    let dogUrl = dogBreed.message;
+    console.log(dogUrl);
+    return dogUrl;
+  }
+
+  let dogetype1 = await dogBreed1();
+
   const body = document.querySelector("body");
   const divMain = document.createElement("div"); //divMainin alle tulee kaikki muut elementit
   divMain.classList.add("container");
@@ -25,7 +36,7 @@ function initializeCode() {
   const img = document.createElement("img"); //Tähän koiran kuva           class.wiki-img
 
   img.classList.add("wiki-img");
-  img.src = "https://images.dog.ceo/breeds/schipperke/n02104365_3826.jpg";
+  img.src = dogetype1;
   divpic.classList.add("img-container"); //Annetaan ylläoleville muuttujille luokat
   divtext.classList.add("wiki-content");
   p.classList.add("wiki-text");
@@ -43,11 +54,22 @@ function initializeCode() {
   divtext.appendChild(divpic);
   divpic.appendChild(img);
 
-  document.getElementById("doggo1").innerHTML = "schipperke"; //Nimetään otsikko ja laitetaan teksti kappaleeseen
+  document.getElementById("doggo1").innerHTML = "Schipperke"; //Nimetään otsikko ja laitetaan teksti kappaleeseen
   document.getElementById("doggo1text").innerHTML =
     "A Schipperke  is a small breed of dog that originated since the 1600's in Belgium. There has been a long informal debate over whether this type of dog is a spitz or miniature sheepdog. In their home country of Belgium they are considered a small shepherd. DNA research has shown that Schipperkes have a close relationship to the Spitz family of dog breeds.";
 
   // Toinen koira alkaa tästä
+
+  async function dogBreed2() {
+    let url = "https://dog.ceo/api/breed/entlebucher/images/random";
+    let response = await fetch(url);
+    let dogBreed = await response.json();
+    let dogUrl = dogBreed.message;
+    console.log(dogUrl);
+    return dogUrl;
+  }
+
+  let dogetype2 = await dogBreed2();
 
   const div1 = document.createElement("div");
   const divtext1 = document.createElement("div");
@@ -57,7 +79,7 @@ function initializeCode() {
   const img1 = document.createElement("img");
 
   img1.classList.add("wiki-img");
-  img1.src = "https://images.dog.ceo/breeds/entlebucher/n02108000_261.jpg";
+  img1.src = dogetype2;
   divpic1.classList.add("img-container");
   divtext1.classList.add("wiki-content");
   p1.classList.add("wiki-text");
@@ -72,11 +94,22 @@ function initializeCode() {
   divtext1.appendChild(p1);
   divtext1.appendChild(divpic1);
   divMain.appendChild(div1);
-  document.getElementById("doggo2").innerHTML = "entlebucher";
+  document.getElementById("doggo2").innerHTML = "Entlebucher";
   document.getElementById("doggo2text").innerHTML =
     "The Entlebucher Sennenhund or Entlebucher Mountain Dog is a medium-sized herding dog, it is the smallest of the four regional breeds that constitute the Sennenhund dog type. The name Sennenhund refers to people called Senn, herders in the Swiss Alps. Entlebuch is a region in the canton of Lucerne in Switzerland. The breed is also known in English as the Entelbuch Mountain Dog, Entelbucher Cattle Dog, and similar combinations.";
 
   // Kolmas koira
+
+  async function dogBreed3() {
+    let url = "https://dog.ceo/api/breed/komondor/images/random";
+    let response = await fetch(url);
+    let dogBreed = await response.json();
+    let dogUrl = dogBreed.message;
+    console.log(dogUrl);
+    return dogUrl;
+  }
+
+  let dogetype3 = await dogBreed3();
 
   const div22 = document.createElement("div");
   const divtext2 = document.createElement("div");
@@ -86,7 +119,7 @@ function initializeCode() {
   const img2 = document.createElement("img");
 
   img2.classList.add("wiki-img");
-  img2.src = "https://images.dog.ceo/breeds/komondor/n02105505_3380.jpg";
+  img2.src = dogetype3;
   divpic2.classList.add("img-container");
   divtext2.classList.add("wiki-content");
   p2.classList.add("wiki-text");
@@ -101,11 +134,22 @@ function initializeCode() {
   divtext2.appendChild(p2);
   divtext2.appendChild(divpic2);
   divMain.appendChild(div22);
-  document.getElementById("doggo3").innerHTML = "komondor";
+  document.getElementById("doggo3").innerHTML = "Komondor";
   document.getElementById("doggo3text").innerHTML =
     "The Komondor also known as the Hungarian sheepdog, is a large, white-coloured Hungarian breed of livestock guardian dog with a long, corded coat.";
 
   //Neljäs koira
+
+  async function dogBreed4() {
+    let url = "https://dog.ceo/api/breed/boxer/images/random";
+    let response = await fetch(url);
+    let dogBreed = await response.json();
+    let dogUrl = dogBreed.message;
+    console.log(dogUrl);
+    return dogUrl;
+  }
+
+  let dogetype4 = await dogBreed4();
 
   const div32 = document.createElement("div");
   const divtext3 = document.createElement("div");
@@ -115,7 +159,7 @@ function initializeCode() {
   const img3 = document.createElement("img");
 
   img3.classList.add("wiki-img");
-  img3.src = "https://images.dog.ceo/breeds/sharpei/noel.jpg";
+  img3.src = dogetype4;
   divpic3.classList.add("img-container");
   divtext3.classList.add("wiki-content");
   p3.classList.add("wiki-text");
@@ -130,11 +174,22 @@ function initializeCode() {
   divtext3.appendChild(p3);
   divtext3.appendChild(divpic3);
   divMain.appendChild(div32);
-  document.getElementById("doggo4").innerHTML = "sharpei";
+  document.getElementById("doggo4").innerHTML = "Boxer";
   document.getElementById("doggo4text").innerHTML =
     "The Shar Pei (Cantonese: shā pèih or Mandarin: 沙皮 shā pí) is a dog breed from southern China. Traditionally kept as a watchdog and property guardian, the Shar Pei was driven to the brink of extinction in the 20th century. The breed is known in the West for its deep wrinkles, whilst a traditional less wrinkled form is maintained in Hong Kong.";
 
   // Viides koira
+
+  async function dogBreed5() {
+    let url = "https://dog.ceo/api/breed/germanshepherd/images/random";
+    let response = await fetch(url);
+    let dogBreed = await response.json();
+    let dogUrl = dogBreed.message;
+    console.log(dogUrl);
+    return dogUrl;
+  }
+
+  let dogetype5 = await dogBreed5();
 
   const div42 = document.createElement("div");
   const divtext4 = document.createElement("div");
@@ -144,7 +199,7 @@ function initializeCode() {
   const img4 = document.createElement("img");
 
   img4.classList.add("wiki-img");
-  img4.src = "https://images.dog.ceo/breeds/germanshepherd/n02106662_15858.jpg";
+  img4.src = dogetype5;
   divpic4.classList.add("img-container");
   divtext4.classList.add("wiki-content");
   p4.classList.add("wiki-text");
@@ -159,7 +214,7 @@ function initializeCode() {
   divtext4.appendChild(p4);
   divtext4.appendChild(divpic4);
   divMain.appendChild(div42);
-  document.getElementById("doggo5").innerHTML = "germanshephard";
+  document.getElementById("doggo5").innerHTML = "Germanshephard";
   document.getElementById("doggo5text").innerHTML =
     "The German Shepherd Dog also known as the Alsatian, is a German breed of working dog of medium to large size. The breed was developed by Max von Stephanitz using various traditional German herding dogs from 1899.";
 }
